921029945c01266bf2412b738f2c5764c4bd569f
awoldt <abreckwoldt23@gmail.com>
2021-04-27 18:15:18 -0400

    icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Music Recommender'),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Text('Connect your Spotify account'),
-        ),
-        // ignore: missing_required_param
-        floatingActionButton: FloatingActionButton(
-          child: Text('Click'),
+      home: Test(),
+    ));
+
+class Test extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Music Recommender'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: IconButton(
+          onPressed: () => {},
+          icon: Icon(Icons.alarm_add_outlined),
+          color: Colors.blue,
         ),
       ),
-    ));
+      // ignore: missing_required_param
+      floatingActionButton: FloatingActionButton(
+        child: Text('click'),
+        onPressed: () {},
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
